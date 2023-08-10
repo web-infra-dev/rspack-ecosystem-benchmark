2aad914b65eff5bda13651c29f62117cf9183be3
jerrykingxyz <jerrykingxyz@gmail.com>
2023-08-10 20:00:42 +0800

    feat: benchmark case add optimization

--- cases/10000/hmr.js
+++ cases/10000/hmr.js
@@ -3,10 +3,8 @@ module.exports = [
 		rebuildChangeFile: "./src/f0.jsx",
 		generateContent: function (originalContent, runTimes) {
 			return (
-				originalContent +
-				`
-console.log(${runTimes});
-`
+				`import "data:text/javascript,export default ${runTimes}";
+` + originalContent
 			);
 		}
 	}
--- cases/10000/rspack.config.js
+++ cases/10000/rspack.config.js
@@ -2,10 +2,20 @@ const path = require("path");
 /** @type {import("@rspack/cli").Configuration} */
 module.exports = {
 	resolve: {
-		extensions: [".js", ".jsx"],
+		extensions: [".js", ".jsx"]
 	},
 	entry: { main: "./index.jsx" },
 	builtins: {
-		html: [{ template: path.resolve(__dirname, "./index.html") }],
+		html: [{ template: path.resolve(__dirname, "./index.html") }]
 	},
+	optimization: {
+		splitChunks: {
+			chunks: "all",
+			cacheGroups: {
+				d1: {
+					test: /\/d1\//
+				}
+			}
+		}
+	}
 };
