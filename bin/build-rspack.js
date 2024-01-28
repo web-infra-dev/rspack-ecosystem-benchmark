d234b77e9d32321b0becfd4228d3fe4a5e68860b
Cong-Cong <dacongsama@live.com>
2024-01-28 23:26:40 +0800

    refactor: simplify async invocation of buildRspack

--- bin/build-rspack.js
+++ bin/build-rspack.js
@@ -2,9 +2,7 @@ import { buildRspack } from "../lib/index.js";
 
 const [, , remote = "origin", branch = "main"] = process.argv;
 
-(async () => {
-	buildRspack(remote, branch);
-})().catch(err => {
+buildRspack(remote, branch).catch(err => {
 	process.exitCode = 1;
 	console.error(err.stack);
 });
