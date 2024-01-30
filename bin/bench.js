9187e8836d6486c5b203ca43667ae21cc2f45447
Cong-Cong Pan <dacongsama@live.com>
2024-01-30 10:21:42 +0800

    feat: add arco pro as a case (#16)
    * feat: add arco-pro as a case
    
    * fix: getDirSizes only get first level size
    
    * feat: bench add arco-pro
    
    * fix: missing dep @turf/flatten

--- bin/bench.js
+++ bin/bench.js
@@ -10,6 +10,9 @@ const defaultBenchmarkNames = [
 	"10000_development-mode",
 	"10000_development-mode_hmr",
 	"10000_production-mode",
+	"arco-pro_development-mode",
+	"arco-pro_development-mode_hmr",
+	"arco-pro_production-mode",
 	"threejs_development-mode_10x",
 	"threejs_development-mode_10x_hmr",
 	"threejs_production-mode_10x"
