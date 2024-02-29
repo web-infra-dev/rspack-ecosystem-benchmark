3b41365a3d9c5634a84741e63f7d23b862897231
Gengkun <ahabhgk@gmail.com>
2024-02-29 14:24:27 +0800

    feat: add intercept plugin addon (#20)

--- bin/bench.js
+++ bin/bench.js
@@ -11,8 +11,11 @@ const defaultBenchmarkNames = [
 	"10000_development-mode_hmr",
 	"10000_production-mode",
 	"arco-pro_development-mode",
+	"arco-pro_development-mode_intercept-plugin",
 	"arco-pro_development-mode_hmr",
+	"arco-pro_development-mode_hmr_intercept-plugin",
 	"arco-pro_production-mode",
+	"arco-pro_production-mode_intercept-plugin",
 	"threejs_development-mode_10x",
 	"threejs_development-mode_10x_hmr",
 	"threejs_production-mode_10x"
