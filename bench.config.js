10be3580507c997a2b5406b0cc1a105ef8e95f36
Cong-Cong <dacongsama@live.com>
2024-11-19 10:37:12 +0800

    fix: only 10000_big case

--- bench.config.js
+++ bench.config.js
@@ -1,16 +1,16 @@
 export default {
 	jobs: [
-		"10000_development-mode",
-		"10000_development-mode_hmr",
-		"10000_production-mode",
+		// "10000_development-mode",
+		// "10000_development-mode_hmr",
+		// "10000_production-mode",
 		"10000_big_production-mode",
-		"arco-pro_development-mode",
-		"arco-pro_development-mode_hmr",
-		"arco-pro_production-mode",
-		"arco-pro_production-mode_generate-package-json-webpack-plugin",
-		"threejs_development-mode_10x",
-		"threejs_development-mode_10x_hmr",
-		"threejs_production-mode_10x"
+		// "arco-pro_development-mode",
+		// "arco-pro_development-mode_hmr",
+		// "arco-pro_production-mode",
+		// "arco-pro_production-mode_generate-package-json-webpack-plugin",
+		// "threejs_development-mode_10x",
+		// "threejs_development-mode_10x_hmr",
+		// "threejs_production-mode_10x"
 	],
 	rspackDirectory: process.env.RSPACK_DIR
 };
