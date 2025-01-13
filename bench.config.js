3e64ce005f28a54d654fd1fca7842d0845c92003
jinrui <jerrykingxyz@gmail.com>
2025-01-13 22:15:22 +0800

    feat: add persistent cold & hot benchmark (#64)

--- bench.config.js
+++ bench.config.js
@@ -3,17 +3,23 @@ export default {
 		"10000_development-mode",
 		"10000_development-mode_hmr",
 		"10000_production-mode",
+		"10000_production-mode_persistent-cold",
+		"10000_production-mode_persistent-hot",
 		"10000_big_production-mode_disable-minimize",
 		"arco-pro_development-mode",
 		"arco-pro_development-mode_hmr",
 		"arco-pro_production-mode",
+		"arco-pro_production-mode_persistent-cold",
+		"arco-pro_production-mode_persistent-hot",
 		"arco-pro_production-mode_traverse-chunk-modules",
 		"arco-pro_production-mode_generate-package-json-webpack-plugin",
 		"large-dyn-imports_development-mode",
 		"large-dyn-imports_production-mode",
 		"threejs_development-mode_10x",
 		"threejs_development-mode_10x_hmr",
-		"threejs_production-mode_10x"
+		"threejs_production-mode_10x",
+		"threejs_production-mode_10x_persistent-cold",
+		"threejs_production-mode_10x_persistent-hot"
 	],
 	rspackDirectory: process.env.RSPACK_DIR
 };
