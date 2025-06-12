1e0acb9a4b052e5c525492a6df493bef038378ad
pshu <stormslowly@gmail.com>
2025-06-12 15:56:54 +0800

    feat: store commit performance data (#67)

--- bin/upload.js
+++ bin/upload.js
@@ -4,16 +4,20 @@ import { resolve, join } from "path";
 import { fileURLToPath } from "url";
 import { runCommand, dirExist, formatDate } from "../lib/utils.js";
 
-const [, , token] = process.argv;
+const [, , token, sha] = process.argv;
+const storeByDate = !sha;
 const GITHUB_ACTOR = process.env.GITHUB_ACTOR;
-const date = formatDate(+new Date());
+const dataTag = storeByDate ? formatDate(+new Date()) : sha.slice(0, 8);
+const relativePath = storeByDate
+	? dataTag
+	: join("commits", sha.slice(0, 2), sha.slice(2));
 const repoUrl = `https://${GITHUB_ACTOR}:${token}@github.com/web-infra-dev/rspack-ecosystem-benchmark.git`;
 
 const rootDir = resolve(fileURLToPath(import.meta.url), "../..");
 const dataDir = resolve(rootDir, ".data");
 const outputDir = resolve(rootDir, "output");
 const rspackDir = process.env.RSPACK_DIR || resolve(rootDir, ".rspack");
-const dateDir = resolve(dataDir, date);
+const storeDir = resolve(dataDir, relativePath);
 
 async function getCommitSHA() {
 	let commitSHA;
@@ -32,7 +36,7 @@ async function appendRspackBuildInfo() {
 	const buildInfo = existsSync(buildInfoFile)
 		? JSON.parse(await readFile(buildInfoFile, "utf-8"))
 		: {};
-	buildInfo[date] = {
+	buildInfo[dataTag] = {
 		commitSHA
 	};
 	await writeFile(buildInfoFile, JSON.stringify(buildInfo, null, 2));
@@ -62,34 +66,39 @@ async function appendRspackBuildInfo() {
 	const files = new Set((await readFile(indexFile, "utf-8")).split("\n"));
 	files.delete("");
 
-	if (!(await dirExist(dateDir))) {
-		await mkdir(dateDir);
+	if (!(await dirExist(storeDir))) {
+		await mkdir(storeDir, { recursive: true });
 	}
 	const outputFiles = await readdir(outputDir);
 	for (const item of outputFiles) {
 		if (item.endsWith(".json")) {
-			files.add(`${date}/${item}`);
+			files.add(`${dataTag}/${item}`);
 		}
-		await copyFile(resolve(outputDir, item), resolve(dateDir, item));
+		await copyFile(resolve(outputDir, item), resolve(storeDir, item));
 	}
 
-	console.log("== update index.txt ==");
-	await writeFile(indexFile, Array.from(files, f => `${f}\n`).join("") + "\n");
+	if (storeByDate) {
+		console.log("== update index.txt ==");
+		await writeFile(
+			indexFile,
+			Array.from(files, f => `${f}\n`).join("") + "\n"
+		);
 
-	console.log("== update build-info.json ==");
-	process.chdir(rspackDir);
-	await appendRspackBuildInfo();
+		console.log("== update build-info.json ==");
+		process.chdir(rspackDir);
+		await appendRspackBuildInfo();
+	}
 	process.chdir(dataDir);
 
 	console.log("== commit ==");
-	await runCommand("git", [
-		"add",
-		`${date}/*.json`,
-		"index.txt",
-		"build-info.json"
-	]);
+	await runCommand(
+		"git",
+		storeByDate
+			? ["add", `${relativePath}/*.json`, "index.txt", "build-info.json"]
+			: ["add", `${relativePath}/*.json`]
+	);
 	try {
-		await runCommand("git", ["commit", "-m", `"add ${date} results"`]);
+		await runCommand("git", ["commit", "-m", `"add ${dataTag} results"`]);
 	} catch {}
 
 	console.log("== push ==");
