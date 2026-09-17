dffe4aa22856e88168f277658be71dc6e173769b
intellild <intellild@outlook.com>
2026-09-17 11:24:56 +0800

    chore(10000): add CSS extraction to benchmark

--- cases/10000/index.jsx
+++ cases/10000/index.jsx
@@ -2,12 +2,10 @@
 import React, { useEffect } from "react";
 import ReactDom from "react-dom/client";
 import App1 from "./src/f0";
+import "./index.css";
 
 ReactDom.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<App1 />
 	</React.StrictMode>
 );
-
-		
-		
--- cases/10000/rspack.config.js
+++ cases/10000/rspack.config.js
@@ -3,15 +3,29 @@ import { fileURLToPath } from "node:url";
 import rspack from "@rspack/core";
 import ReactRefreshPlugin from "@rspack/plugin-react-refresh";
 
+const CSS_LOADER = {
+	loader: "css-loader",
+	options: {
+		modules: { auto: true, namedExport: false, exportLocalsConvention: "as-is" }
+	}
+};
+
 const prod = process.env.NODE_ENV === "production";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 /** @type {import("@rspack/cli").Configuration} */
 const config = {
+	experiments: {
+		css: false
+	},
 	resolve: {
 		extensions: [".js", ".jsx"]
 	},
 	entry: { main: "./index.jsx" },
 	plugins: [
+		new rspack.CssExtractRspackPlugin({
+			filename: "static/css/[name].[contenthash:8].css",
+			chunkFilename: "static/css/async/[name].[contenthash:8].css"
+		}),
 		new rspack.HtmlRspackPlugin({
 			template: path.resolve(__dirname, "./index.html")
 		}),
@@ -19,6 +33,13 @@ const config = {
 	].filter(Boolean),
 	module: {
 		rules: [
+			{
+				test: /\.css$/,
+				type: "javascript/auto",
+				sideEffects: true,
+				use: [rspack.CssExtractRspackPlugin.loader, CSS_LOADER],
+				resolve: { preferRelative: true }
+			},
 			{
 				test: /\.(j|t)s$/,
 				exclude: [/[\\/]node_modules[\\/]/],
